11b5b0e5792152cb073689ca14d4c528b0c91508
Ivanlvanov <donawqa@gmail.com>
2025-11-10 21:56:07 +0100

    Add Custom WebAppFactory instead of Postman

---
+++ HotelBooking.IntegrationTests/CreateBookingTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using HotelBooking.Infrastructure;
+using HotelBooking.Core.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace HotelBooking.IntegrationTests;
+
+public class CreateBookingTests : IClassFixture<CustomWebAppFactory<Program>>, IDisposable
+{
+    private readonly CustomWebAppFactory<Program> _factory;
+
+    public CreateBookingTests(CustomWebAppFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    public void Dispose()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var db = services.GetRequiredService<HotelBookingContext>();
+        db.Database.EnsureDeleted();
+        GC.SuppressFinalize(this);
+    }
+
+
+    [Fact]
+    public async Task BookingFails_WhenEndDateBeforeStartDate()
+    {
+        var booking = new
+        {
+            StartDate = DateTime.Today.AddDays(2),
+            EndDate = DateTime.Today.AddDays(1),
+            CustomerId = 1
+        };
+
+
+    }
+
+    [Fact]
+    public async Task BookingSucceeds_WhenValidDatesBeforeOccupied()
+    {
+        var booking = new
+        {
+            StartDate = DateTime.Today.AddDays(1),
+            EndDate = DateTime.Today.AddDays(2),
+            CustomerId = 1
+        };
+    }
+}
---
+++ HotelBooking.IntegrationTests/CustomWebFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HotelBooking.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HotelBooking.IntegrationTests;
+
+
+public class CustomWebAppFactory<T> : WebApplicationFactory<T> where T : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<HotelBookingContext>));
+            
+                services.Remove(descriptor);
+
+            services.AddDbContext<HotelBookingContext>(options =>
+            {
+                options.UseInMemoryDatabase("HotelBookingTestDb");
+            });
+        });
+        
+        builder.UseEnvironment("Development");
+    }
+}
